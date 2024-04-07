3cc16ba36edfe9b83e5de90569a2131259e1818c
MarkWu26 <markwu1230@gmail.com>
2024-04-07 13:22:59 +0800

    fix

--- components/AccountMenu.tsx
+++ components/AccountMenu.tsx
@@ -17,7 +17,7 @@ const AccountMenu: React.FC<AccountMenuProps> = ({ visible, currentUser }) => {
   }
 
   return (
-    <div className="bg-black w-56 absolute top-14 right-0 py-5 flex-col border-2 border-gray-800 flex">
+    <div className="bg-black w-56 absolute top-14 right-0 py-5 flex-col border-2 border-gray-800 flex rounded-md">
       <div className="flex flex-col gap-3">
         <div className="px-3 group/item flex flex-row gap-3 items-center w-full">
           <img className="w-8 rounded-md" src="/images/default-blue.png" alt="" />
